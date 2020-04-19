c261076aa902dd70249bc164dd305a6b3010ab1c
Vasil <vvpanayotovv@gmail.com>
2020-04-19 21:48:48 +0300

    last problem from the exam made it with 6 :D

---
+++ ExerciseOldStuff/src/functionalProgramming/ConsumerPrint.java
@@ -0,0 +1,11 @@
+package functionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(System.out::println);
+    }
+}
