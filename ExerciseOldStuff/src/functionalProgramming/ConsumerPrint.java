36cf612db1b59ba2987307c028ed63d52df3e21c
Vasil <vvpanayotovv@gmail.com>
2020-04-19 21:50:46 +0300

    exercise

--- ExerciseOldStuff/src/functionalProgramming/ConsumerPrint.java
+++ ExerciseOldStuff/src/functionalProgramming/ConsumerPrint.java
@@ -2,10 +2,12 @@ package functionalProgramming;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class ConsumerPrint {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(System.out::println);
+        Consumer<String> cons = System.out::println;
+        Arrays.stream(scanner.nextLine().split("\\s+")).forEach(cons);
     }
 }
