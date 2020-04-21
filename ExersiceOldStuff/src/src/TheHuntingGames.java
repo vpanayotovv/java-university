2a454adc44fed73e5aab22063eff89f0d12156c4
Vasil <vvpanayotovv@gmail.com>
2020-04-21 16:13:50 +0300

    update

---
+++ ExersiceOldStuff/src/src/TheHuntingGames.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.util.Scanner;
+
+public class TheHuntingGames{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int daysOfAdv = Integer.parseInt(scanner.nextLine());
+        int countOfPlayers = Integer.parseInt(scanner.nextLine());
+        double energy =Double.parseDouble (scanner.nextLine());
+        double waterPerDay = Double.parseDouble(scanner.nextLine());
+        double totalWater = waterPerDay * daysOfAdv * countOfPlayers;
+        double foodPerDay = Double.parseDouble(scanner.nextLine());
+        double totalFood = foodPerDay * daysOfAdv * countOfPlayers;
+        boolean isDone = true;
+        for (int i = 1; i <= daysOfAdv; i++) {
+           double energyLost = Double.parseDouble(scanner.nextLine());
+           energy -= energyLost;
+           if (energy <= 0){
+               isDone = false;
+               break;
+           }
+           if (i % 2 ==0){
+               energy += energy * 0.05;
+               totalWater -= totalWater * 0.3;
+           }
+           if (i % 3 == 0){
+               energy  += energy * 0.1;
+               totalFood -= totalFood / (double) countOfPlayers;
+           }
+        }
+
+        if (isDone){
+            System.out.println(String.format("You are ready for the quest. You will be left with - %.2f energy!",energy));
+        }else {
+            System.out.println(String.format("You will run out of energy. You will be left with %.2f food and %.2f water.",totalFood,totalWater));
+        }
+
+    }
+}
---
+++ ExersiceOldStuff/src/src/functionalProgramming/AppliedArithmetic.java
@@ -0,0 +1,50 @@
+package src.functionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class AppliedArithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        Function<List<Integer>, List<Integer>> functionAdd = list -> list.stream()
+                .map(e -> e + 1)
+                .collect(Collectors.toList());
+        Function<List<Integer>, List<Integer>> functionMultiply = list -> list.stream()
+                .map(e -> e * 2)
+                .collect(Collectors.toList());
+        Function<List<Integer>, List<Integer>> functionSubtract = list -> list.stream()
+                .map(e -> e - 1)
+                .collect(Collectors.toList());
+        Function<List<Integer>, List<Integer>> functionPrint = list -> list.stream()
+                .peek(e -> System.out.print(e + " "))
+                .collect(Collectors.toList());
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            String command = input;
+            switch (command) {
+                case "add":
+                    numbers = functionAdd.apply(numbers);
+                    break;
+                case "multiply":
+                    numbers = functionMultiply.apply(numbers);
+                    break;
+                case "subtract":
+                    numbers = functionSubtract.apply(numbers);
+                    break;
+                default:
+                    numbers = functionPrint.apply(numbers);
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
