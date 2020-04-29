567a948ce30882fe8806c1e3125168209458f0a0
Vasil <vvpanayotovv@gmail.com>
2020-04-29 11:59:10 +0300

    update Arrays.js

--- Java Script Advaced/Arrays.js
+++ Java Script Advaced/Arrays.js
@@ -1,50 +1,60 @@
 function print(input) {
     let separator = input[input.length - 1]
-    input.splice(input.length - 1,1)
+    input.splice(input.length - 1, 1)
     console.log(input.join(separator))
 }
 
-function printNum(input){
+function printNum(input) {
     let lastNum = +input.pop();
-    for (let i = 0; i < input.length; i+= lastNum) {
+    for (let i = 0; i < input.length; i += lastNum) {
         console.log(input[i])
     }
 }
 
-function addAndRemove(input){
+function addAndRemove(input) {
     let counter = 0;
     let newArr = [];
     for (let i = 0; i < input.length; i++) {
         counter++;
-        if(input[i] === 'add'){
+        if (input[i] === 'add') {
             newArr.push(counter);
-        } else if (input[i] === 'remove'){
+        } else if (input[i] === 'remove') {
             newArr.pop();
         }
     }
-    if(newArr.length == 0){
+    if (newArr.length == 0) {
         return console.log('Empty')
-    }else{
-    
-    return console.log(newArr.join('\n').trim());
+    } else {
+
+        return console.log(newArr.join('\n').trim());
     }
 }
-function rotateArray(input){
+function rotateArray(input) {
     let rotations = (+input[input.length - 1]) % (input.length - 1)
-    let arr = input.slice(0,input.length-1)
+    let arr = input.slice(0, input.length - 1)
     for (let i = 0; i < +rotations; i++) {
-        arr.unshift(arr.pop()   )
+        arr.unshift(arr.pop())
     }
     console.log(arr.join(' '))
 }
-function extract(input){
+function extract(input) {
 
-   let output = input.reduce((acc,curr) => {
+    let output = input.reduce((acc, curr) => {
         const lastElement = acc[acc.length - 1]
-        if(curr >= lastElement || lastElement === undefined){
+        if (curr >= lastElement || lastElement === undefined) {
             acc.push(curr);
         }
         return acc;
-    },[])
+    }, [])
     console.log(output.join('\n'))
+}
+function sortString(input) {
+    input.sort((a, b) => {
+        if (a.length === b.length) {
+            return a.localeCompare(b)
+        } else {
+            return a.length - b.length
+        }
+    })
+    console.log(input.join('\n'))
 }
