a24a9ba54c62d53e7ac38038d8bde269ff73b720
Vasil <vvpanayotovv@gmail.com>
2020-04-29 16:06:20 +0300

    update Arrays.js

--- Java Script Advaced/Arrays.js
+++ Java Script Advaced/Arrays.js
@@ -58,3 +58,21 @@ function sortString(input) {
     })
     console.log(input.join('\n'))
 }
+function magicMatrix(matrix) {
+    for (let i = 0; i < matrix.length - 1; i++) {
+        let sumROne = matrix[i].reduce((a, b) => a + b, 0);
+        let sumRTwo = matrix[i + 1].reduce((a, b) => a + b, 0);
+        let sumCOne = 0;
+        let sumCTwo = 0;
+
+        for (let j = 0; j < matrix.length; j++) {
+            sumCOne += matrix[i][j];
+            sumCTwo += matrix[i + 1][j];
+        }
+
+        if (sumROne !== sumRTwo || sumCOne !== sumCTwo) {
+            return console.log('false');
+        }
+    }
+    return console.log('true');
+}
