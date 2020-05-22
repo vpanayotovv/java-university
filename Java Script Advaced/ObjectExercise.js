bdefc9966f3b8e831048da0dbaa14025de5e08ab
Vasil <vvpanayotovv@gmail.com>
2020-05-22 16:56:59 +0300

    problemsolving

---
+++ Java Script Advaced/ObjectExercise.js
@@ -0,0 +1,10 @@
+function solve(input){
+    let heroes = [];
+    for (let line of input) {
+        let[name,level,items] = line.split(' / ');
+        level = Number(level);
+        items = items ? items.split(', ') : [] ;
+        heroes.push({name,level,items})
+    }
+    return JSON.stringify(heroes);
+}
