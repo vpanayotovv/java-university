4ab8e2797674cdc63c3ca2a4d9f60091e04986bd
Vasil <vvpanayotovv@gmail.com>
2020-05-23 13:22:52 +0300

    up

--- Java Script Advaced/ObjectExercise.js
+++ Java Script Advaced/ObjectExercise.js
@@ -56,9 +56,3 @@ function secquence(input){
     .join('\n')
     return output
 }
-
-console.log(secquence([
-    "[-3, -2, -1, 0, 1, 2, 3, 4]",
-"[10, 1, -17, 0, 2, 13]",
-"[4, -3, 3, -2, 2, -1, 1, 0]"
-]))
