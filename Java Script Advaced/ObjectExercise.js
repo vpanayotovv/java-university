4fb4e40f380588fd881bf53ae7ff9936b56be8d8
Vasil <vvpanayotovv@gmail.com>
2020-05-23 13:19:11 +0300

    problem04

--- Java Script Advaced/ObjectExercise.js
+++ Java Script Advaced/ObjectExercise.js
@@ -41,3 +41,24 @@ function sort(input){
     }
     return Object.keys(products).sort();
 }
+function secquence(input){
+    let nestedArrays = input.map(JSON.parse);
+    let uniqueArrays = [];
+    for (let arr of nestedArrays) {
+        let sum = arr.reduce((a,b) => a + b,0);
+        let cotainsSum = uniqueArrays.some(x => x.reduce((a,b) => a + b,0) === sum);
+        if(!cotainsSum){
+            uniqueArrays.push(arr.sort((a,b) => b - a))
+        }
+    }
+    let output = uniqueArrays.sort((a,b) => a.length - b.length)
+    .map(arr => `[${arr.join(", ")}]`)
+    .join('\n')
+    return output
+}
+
+console.log(secquence([
+    "[-3, -2, -1, 0, 1, 2, 3, 4]",
+"[10, 1, -17, 0, 2, 13]",
+"[4, -3, 3, -2, 2, -1, 1, 0]"
+]))
