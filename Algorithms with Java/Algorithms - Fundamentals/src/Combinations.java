5013174def056040e79ac73c783b34079710e765
Vasil <vvpanayotovv@gmail.com>
2020-05-30 14:34:38 +0300

    variations

---
+++ Algorithms with Java/Algorithms - Fundamentals/src/Combinations.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Combinations {
+    private static String[] elements;
+    private static String[] variations;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        elements = scanner.nextLine().split("\\s+");
+        variations = new String[Integer.parseInt(scanner.nextLine())];
+
+        combinations(0, 0);
+
+    }
+
+    private static void combinations(int index, int start) {
+        if (index == variations.length) {
+            print(variations);
+            return;
+        }
+        for (int i = start; i < elements.length; i++) {
+            variations[index] = elements[i];
+            combinations(index + 1, i + 1);
+        }
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ", arr));
+    }
+}
