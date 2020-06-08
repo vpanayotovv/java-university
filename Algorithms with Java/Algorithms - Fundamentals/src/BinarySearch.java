fa0102346dfd86ee583bf98aed266c00600ba4e3
Vasil <vvpanayotovv@gmail.com>
2020-06-08 17:15:08 +0300

    simple bst

---
+++ Algorithms with Java/Algorithms - Fundamentals/src/BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int key = Integer.parseInt(scanner.nextLine());
+        System.out.println(getIndex(array, key));
+    }
+
+    private static int getIndex(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int curr = arr[mid];
+            if (key < curr) {
+                end = mid - 1;
+
+            } else if (key > curr) {
+                start = mid + 1;
+
+            } else {
+                return mid;
+
+            }
+        }
+        return -1;
+    }
+}
