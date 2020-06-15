1f554bd62e8afbe1bc7ed831f19a1ed11f5078ee
Vasil <vvpanayotovv@gmail.com>
2020-06-16 00:08:37 +0300

    Topological sort for graphs

---
+++ Algorithms with Java/Algorithms - Fundamentals/Graph/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Collection;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        throw new AssertionError("Not Implemented");
+    }
+
+    public static Collection<String> topSort(Map<String, List<String>> graph) {
+        throw new AssertionError("Not Implemented");
+    }
+}
