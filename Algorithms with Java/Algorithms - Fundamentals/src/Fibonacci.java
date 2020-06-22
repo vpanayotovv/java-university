0cf3a37c26ae54a8ecd854f0b87c3a1ba6ca33bb
Vasil <vvpanayotovv@gmail.com>
2020-06-22 11:55:01 +0300

    Dynamic programming

---
+++ Algorithms with Java/Algorithms - Fundamentals/src/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        long fib = calcFib(n);
+
+        System.out.println(fib);
+    }
+
+    private static long calcFib(int n) {
+        if (n <= 2){
+            return 1;
+        }
+
+        return calcFib(n -1 ) + calcFib( n - 2);
+    }
+}
