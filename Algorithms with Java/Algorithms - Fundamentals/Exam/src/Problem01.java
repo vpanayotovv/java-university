e7276fc63c2aac85457da0c301148ebe2665a9b0
Vasil <vvpanayotovv@gmail.com>
2020-06-28 09:39:31 +0300

    100 / 100

---
+++ Algorithms with Java/Algorithms - Fundamentals/Exam/src/Problem01.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    private static String[] elements;
+    private static String[] variations;
+    private static boolean[] used;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        elements = scanner.nextLine().split("\\s+");
+        variations = new String[Integer.parseInt(scanner.nextLine())];
+        used = new boolean[elements.length];
+
+        variations(0);
+
+    }
+
+    private static void variations(int index) {
+        if (index == variations.length){
+            print(variations);
+            return;
+        }
+        for (int i = 0; i < elements.length; i++) {
+            if (!used[i]){
+                used[i] = true;
+                variations[index] = elements[i];
+                variations(index + 1);
+                used[i] = false;
+            }
+        }
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ",arr));
+    }
+}
