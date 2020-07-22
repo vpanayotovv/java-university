f5409cc4b163fd25ea4a4aec1f190578e543ac3b
Vasil <vvpanayotovv@gmail.com>
2020-07-22 11:49:33 +0300

    add skelet

---
+++ Algorithms with Java/Algorithms - Advanced/Graphs-Strongly-Connected-Components-Max-Flow-Skeleton/src/main/java/StronglyConnectedComponents.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class StronglyConnectedComponents {
+
+    public static List<List<Integer>> findStronglyConnectedComponents(List<Integer>[] targetGraph) {
+        return null;
+    }
+}
