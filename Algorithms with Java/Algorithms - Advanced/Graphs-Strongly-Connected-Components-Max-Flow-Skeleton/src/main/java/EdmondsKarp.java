d55980cec484bc669c62c9b45c173b5d39b6c121
Vasil <vvpanayotovv@gmail.com>
2020-07-22 16:49:33 +0300

    implemented

--- Algorithms with Java/Algorithms - Advanced/Graphs-Strongly-Connected-Components-Max-Flow-Skeleton/src/main/java/EdmondsKarp.java
+++ Algorithms with Java/Algorithms - Advanced/Graphs-Strongly-Connected-Components-Max-Flow-Skeleton/src/main/java/EdmondsKarp.java
@@ -1,5 +1,61 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 public class EdmondsKarp {
-    public static int findMaxFlow(int[][] targetGraph) {
-        return -1;
+
+    private static int[][] graph;
+    private static int[] parents;
+    private static boolean[] visited;
+
+    static int findMaxFlow(int[][] targetGraph) {
+
+        graph = targetGraph;
+        parents = new int[graph.length];
+        Arrays.fill(parents, -1);
+        visited = new boolean[graph.length];
+
+        int maxFlow = 0;
+
+        while (bfs()) {
+            int node = graph.length - 1;
+            int flow = Integer.MAX_VALUE;
+            while (node != 0) {
+                flow = Math.min(flow, graph[parents[node]][node]);
+                node = parents[node];
+            }
+            maxFlow += flow;
+            node = graph.length - 1;
+
+            while (node != 0) {
+                graph[parents[node]][node] -= flow;
+                graph[node][parents[node]] += flow;
+                node = parents[node];
+            }
+        }
+
+        return maxFlow;
+    }
+
+    private static boolean bfs() {
+        Deque<Integer> queue = new ArrayDeque<>();
+        Arrays.fill(visited, false);
+        Arrays.fill(parents, -1);
+        queue.offer(0);
+        visited[0] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int child = 0; child < graph.length; child++) {
+                if (graph[node][child] > 0 && !visited[child]) {
+                    visited[child] = true;
+                    parents[child] = node;
+                    queue.offer(child);
+                }
+            }
+
+        }
+
+        return visited[visited.length - 1];
     }
 }
