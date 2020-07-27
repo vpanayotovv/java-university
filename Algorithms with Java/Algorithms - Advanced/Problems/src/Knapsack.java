700ec1e4a4e4361d42da8b4f2bc8bc5215b4efb3
Vasil <vvpanayotovv@gmail.com>
2020-07-27 09:02:41 +0300

    knapsack problem

--- Algorithms with Java/Algorithms - Advanced/Problems/src/Knapsack.java
+++ Algorithms with Java/Algorithms - Advanced/Problems/src/Knapsack.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,6 +7,7 @@ public class Knapsack {
     private static List<Integer> weights = new ArrayList<>();
     private static List<Integer> prices = new ArrayList<>();
     private static int capacity;
+    private static int[][] dp;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,18 +22,25 @@ public class Knapsack {
 
             line = scanner.nextLine();
         }
+        dp = new int[prices.size() + 1][capacity + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
-        int result = recurrence(0,0,capacity);
+        int result = recurrence(0, 0, capacity);
         System.out.println(result);
     }
 
-    private static int recurrence(int valueIndex , int weightIndex,int capacity) {
-        if (valueIndex >= prices.size() || weightIndex >= weights.size() || weights.get(weightIndex) > capacity ) {
+    private static int recurrence(int valueIndex, int weightIndex, int capacity) {
+        if (valueIndex >= prices.size() || weightIndex >= weights.size() || weights.get(weightIndex) > capacity) {
             return 0;
         }
+        if (dp[valueIndex][capacity] != -1) {
+            return dp[valueIndex][capacity];
+        }
 
-        return Math.max(recurrence(valueIndex + 1,weightIndex + 1,capacity),
-                recurrence(valueIndex + 1,weightIndex + 1,capacity - weights.get(weightIndex))
+        return Math.max(recurrence(valueIndex + 1, weightIndex + 1, capacity),
+                recurrence(valueIndex + 1, weightIndex + 1, capacity - weights.get(weightIndex))
                         + prices.get(valueIndex));
     }
 }
