15c94014d33bc59f7391563c1fff908ae93ab4ab
Vasil <vvpanayotovv@gmail.com>
2020-09-18 14:25:49 +0300

    up

--- MySQL/DateTypes.sql
+++ MySQL/DateTypes.sql
@@ -87,3 +87,14 @@ values
 (4,'Software Development'),
 (5,'Quality Assurance');
 
+insert into employees(id,first_name,middle_name,last_name,job_title,department_id,hire_date,salary)
+values
+(1,'Ivan','Ivanov','Ivanov','.NET Developer',4,'2013-02-01',3500.00),
+(2,'Petar','Petrov','Petrov','Senior Engineer',1,'2004-03-02',4000.00),
+(3,'Maria','Petrova','Ivanova','Intern',5,'2016-08-28',525.25),
+(4,'Georgi','Terziev','Ivanov','CEO',2,'2007-12-09',3000.00),
+(5,'Peter','Pan','Pan','Intern',3,'2016-08-28',599.88);
+
+select * from towns;
+select * from departments;
+select * from employees;
