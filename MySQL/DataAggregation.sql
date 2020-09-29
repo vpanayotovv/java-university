3bf2b0d424351ade43458105a187f250e91e577a
Vasil <vvpanayotovv@gmail.com>
2020-09-30 00:48:43 +0300

    subqueries

--- MySQL/DataAggregation.sql
+++ MySQL/DataAggregation.sql
@@ -57,20 +57,87 @@ select case
            when age between 41 and 50 then '[41-50]'
            when age between 51 and 60 then '[51-60]'
            else '[61+]'
-           end as 'age_group',
+           end  as 'age_group',
        count(*) as wizard_count
 from wizzard_deposits
 group by age_group
 order by age_group;
 
-select distinct LEFT(first_name,1) as first_letter
+select distinct LEFT(first_name, 1) as first_letter
 from wizzard_deposits
 where deposit_group = 'Troll Chest'
 order by first_letter;
 
-select deposit_group ,
-       IF(is_deposit_expired is false, '0', '1') as is_deposit_expired,avg(deposit_interest) as average_intrest
+select deposit_group,
+       IF(is_deposit_expired is false, '0', '1') as is_deposit_expired,
+       avg(deposit_interest)                     as average_intrest
 from wizzard_deposits
 where deposit_start_date > '1985-01-01'
-group by deposit_group,is_deposit_expired
-order by deposit_group desc , is_deposit_expired;
+group by deposit_group, is_deposit_expired
+order by deposit_group desc, is_deposit_expired;
+
+use soft_uni;
+
+select department_id, min(salary) as min_salary
+from employees
+where hire_date > '2000-01-01'
+  and department_id in (2, 5, 7)
+group by department_id;
+
+create table highPayedEmployees as
+select department_id, salary
+from employees
+where salary > 30000
+  and manager_id != 42;
+
+update highPayedEmployees
+set salary = salary + 5000
+where department_id = 1;
+
+select department_id, AVG(salary) as avg_salary
+from highPayedEmployees
+group by department_id
+order by department_id;
+
+select department_id, max(salary) as max_salary
+from employees
+group by department_id
+having max_salary not between 30000 and 70000
+order by department_id;
+
+select count(*) as shefovete
+from employees
+where manager_id is null;
+
+select distinct salary
+from employees
+where department_id = 1
+order by salary desc
+limit 2,1;
+
+select e.department_id,
+       (   select distinct e2.salary
+           from employees as e2
+           where e2.department_id = e.department_id
+           order by e2.salary desc
+           limit 2,1
+        ) as 'third_highest_salary'
+from employees as e
+group by e.department_id
+having third_highest_salary is not null
+order by e.department_id;
+
+select e.first_name ,e.last_name ,e.department_id
+from employees as e
+where e.salary >
+    (select avg(salary) as avg_salary
+    from employees as e2
+    where e2.department_id = e.department_id
+    group by e2.department_id)
+order by e.department_id,e.employee_id
+limit 10;
+
+select department_id, sum(salary) as total_salary
+from employees
+group by department_id
+order by department_id;
