c8edade3e5a15f72a7f6cb7ef0643b2cce65f67f
Vasil <vvpanayotovv@gmail.com>
2020-10-05 13:52:42 +0300

    problem solved

--- MySQL/TableRelations.sql
+++ MySQL/TableRelations.sql
@@ -102,3 +102,57 @@ values
 ('3','103'),
 ('2','102'),
 ('2','103');
+
+create table cities(
+  city_id int(11) auto_increment primary key,
+  name varchar(50)
+);
+
+create table customers(
+    customer_id int(11) auto_increment primary key,
+    name varchar(50),
+    birthday date,
+    city_id int(11),
+
+    constraint fk_customers_cities
+    foreign key (city_id)
+    references cities(city_id)
+);
+
+create table orders(
+    order_id int(11) auto_increment primary key,
+    customer_id int(11),
+    constraint fk_orders_customers
+    foreign key (customer_id)
+    references customers(customer_id)
+);
+
+create table item_types(
+    item_type_id int(11) auto_increment primary key,
+    name varchar(50)
+);
+
+create table items(
+    item_id int(11) auto_increment primary key,
+    name varchar(50),
+    item_type_id int (11),
+    constraint fk_items_item_types
+    foreign key (item_type_id)
+    references item_types(item_type_id)
+);
+
+create table order_items(
+    order_id int(11),
+    item_id int(11),
+
+    constraint pk_order_items
+    primary key (order_id,item_id),
+
+    constraint fk_order_items_items
+    foreign key (item_id)
+    references items(item_id),
+
+    constraint fk_order_items_orders
+    foreign key (order_id)
+    references orders(order_id)
+);
