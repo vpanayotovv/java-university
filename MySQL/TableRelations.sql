211b8f6e998c41329b46a1b82d7482f127c7dc39
Vasil <vvpanayotovv@gmail.com>
2020-10-05 10:47:18 +0300

    up

--- MySQL/TableRelations.sql
+++ MySQL/TableRelations.sql
@@ -25,4 +25,34 @@ add constraint fk_persons_passports
 foreign key people(passport_id)
 references passports(passport_id);
 
-use geography;
+create table manufacturers(
+    manufacturer_id int primary key auto_increment unique,
+    name varchar(10),
+    established_on date
+);
+
+insert into manufacturers(manufacturer_id, name, established_on)
+values
+('1','BMW','1916-03-01'),
+('2','Tesla','2003-01-01'),
+('3','Lada','1966-05-01');
+
+create table models(
+    model_id int unique,
+    name varchar(10),
+    manufacturer_id int
+);
+
+insert into models (model_id, name, manufacturer_id)
+values
+       ('101','X1','1'),
+       ('102','i6','1'),
+       ('103','Model S','2'),
+       ('104','Model X','2'),
+       ('105','Model 3','2'),
+       ('106','Nova','3');
+
+alter table models
+add constraint fk_models_manufacturers
+foreign key models(manufacturer_id)
+references manufacturers(manufacturer_id);
