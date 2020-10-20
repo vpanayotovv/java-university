61f15c0d12425b172ab0300ca5e78ff2459de79e
Vasil <vvpanayotovv@gmail.com>
2020-10-20 17:22:17 +0300

    up

--- JDBC/src/demo/Main.java
+++ JDBC/src/demo/Main.java
@@ -1,10 +1,13 @@
 package demo;
 
-import java.sql.SQLOutput;
+import java.sql.*;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
     public static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static String DB_URL = "jdbc:mysql://localhost:3306/soft_uni";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Username:");
@@ -14,13 +17,61 @@ public class Main {
         String password = scanner.nextLine().trim();
         password = password.length() > 0 ? password : "";
 
+        Connection connection = null;
+
         try {
             Class.forName(DB_DRIVER);
         } catch (ClassNotFoundException e) {
-            System.out.println(String.format("%s Not Found!",DB_DRIVER));
+            System.out.println(String.format("%s Not Found!", DB_DRIVER));
             System.exit(0);
         }
 
         System.out.println("DB Driver loaded successfully!");
+
+        Properties properties = new Properties();
+
+        properties.setProperty("user", username);
+        properties.setProperty("password", password);
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, properties);
+        } catch (SQLException e) {
+            System.out.println("Cannot connect DB!");
+            System.exit(0);
+        }
+
+        System.out.println("Connection created successfully!");
+
+        System.out.println("Enter minimum Salary:");
+        String salary = scanner.nextLine().trim();
+        salary = salary.length() > 0 ? salary : "40000";
+        double salaryInt = 40000;
+
+        salaryInt = Double.parseDouble(salary);
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement("select * from soft_uni.employees where salary > ? order by employee_id");
+
+            stmt.setDouble(1,salaryInt);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                System.out.printf("| %10d | %-15.15s | %-15.15s | %10.2f |%n",
+                        rs.getLong("employee_id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getDouble("salary")
+                        );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Error closing connection!");
+        }
     }
 }
