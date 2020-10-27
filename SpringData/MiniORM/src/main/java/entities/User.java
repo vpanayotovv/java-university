868bf95289d7c0bca93ce2a46039e28cf348b5c2
Vasil <vvpanayotovv@gmail.com>
2020-10-27 13:06:02 +0200

    mini orm

---
+++ SpringData/MiniORM/src/main/java/entities/User.java
@@ -0,0 +1,68 @@
+package entities;
+
+public class User {
+             private int id;
+             private String username;
+             private String password;
+             private int age;
+             private String registrationDate;
+
+    public User(int id, String username, String password, int age, String registrationDate) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getRegistrationDate() {
+        return this.registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", registrationDate='" + registrationDate + '\'' +
+                '}';
+    }
+}
