3c9ab734658c7cd94255c27a2e4f09192012f675
Vasil <vvpanayotovv@gmail.com>
2020-10-28 16:32:58 +0200

    create entity

--- SpringData/HibernateLab/src/main/java/springdata/hibernate/entity/Main.java
+++ SpringData/HibernateLab/src/main/java/springdata/hibernate/entity/Main.java
@@ -1,7 +1,26 @@
 package springdata.hibernate.entity;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+        Configuration cfg = new Configuration();
+        cfg.configure();
 
+        SessionFactory sf = cfg.buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        Student student = new Student("Hristo Georgiev");
+        session.beginTransaction();
+
+        session.save(student);
+
+        session.getTransaction().commit();
+
+
+        session.close();
     }
 }
