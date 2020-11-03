70bb7ebad34854f78bb0224c87a4306a9d9d2976
Vasil <vvpanayotovv@gmail.com>
2020-11-03 16:05:17 +0200

    Exercising

--- SpringData/CodeFirstExercise/src/main/java/app/sales/entities/Customer.java
+++ SpringData/CodeFirstExercise/src/main/java/app/sales/entities/Customer.java
@@ -1,4 +1,46 @@
 package app.sales.entities;
 
-public class Customer {
+import app.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    String name;
+    String email;
+    String creditCardNumber;
+
+    public Customer() {
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "credit_card_number")
+    public String getCreditCardNumber() {
+        return this.creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
 }
