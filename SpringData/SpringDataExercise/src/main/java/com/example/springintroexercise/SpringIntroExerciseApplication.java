e392cf11ff108de9a94f1c311c85c671533e411c
Vasil <vvpanayotovv@gmail.com>
2020-11-07 20:49:18 +0200

    config Spring

---
+++ SpringData/SpringDataExercise/src/main/java/com/example/springintroexercise/SpringIntroExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintroexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroExerciseApplication.class, args);
+    }
+
+}
---
+++ SpringData/SpringDataExercise/src/test/java/com/example/springintroexercise/SpringIntroExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintroexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
