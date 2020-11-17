b3050d76a61c9a523f5ef9eee91859eb23834744
Vasil <vvpanayotovv@gmail.com>
2020-11-17 15:14:10 +0200

    config done

---
+++ SpringData/JsonExercise/json-exercise/src/test/java/softuni/jsonexercise/JsonExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.jsonexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
