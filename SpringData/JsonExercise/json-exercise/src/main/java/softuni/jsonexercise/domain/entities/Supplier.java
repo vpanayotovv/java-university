bb7c464ff222fc752cadf8e2a1ba7b77c6d6a61e
Vasil <vvpanayotovv@gmail.com>
2020-11-17 16:57:34 +0200

    add supplier

---
+++ SpringData/JsonExercise/json-exercise/src/main/java/softuni/jsonexercise/domain/entities/Supplier.java
@@ -0,0 +1,28 @@
+package softuni.jsonexercise.domain.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "suppliers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private boolean isImporter;
+}
