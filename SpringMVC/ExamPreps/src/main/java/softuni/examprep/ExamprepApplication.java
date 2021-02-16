67ea4f65e6235d6a285b223e0151a24d8771e2f8
Vasil <vvpanayotovv@gmail.com>
2021-02-16 10:02:32 +0200

    initial commit

---
+++ SpringMVC/ExamPreps/src/main/java/softuni/examprep/ExamprepApplication.java
@@ -0,0 +1,13 @@
+package softuni.examprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamprepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamprepApplication.class, args);
+    }
+
+}
---
+++ SpringMVC/ExamPreps/src/test/java/softuni/examprep/ExamprepApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.examprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamprepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
