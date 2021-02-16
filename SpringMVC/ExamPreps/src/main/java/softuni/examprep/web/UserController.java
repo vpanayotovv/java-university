ede68958a800d565588867efe65916fa65d10db3
Vasil <vvpanayotovv@gmail.com>
2021-02-16 14:41:32 +0200

    user controller init

---
+++ SpringMVC/ExamPreps/src/main/java/softuni/examprep/web/UserController.java
@@ -0,0 +1,17 @@
+package softuni.examprep.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/register")
+    public String register(){
+
+        return "register";
+    }
+
+}
