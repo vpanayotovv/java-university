a5a5e01f35267f5e7722c309b7b911401d1ddad8
Vasil <vvpanayotovv@gmail.com>
2021-02-18 16:07:00 +0200

    initial commit

---
+++ SpringMVC/ExamPrep02/src/main/java/com/examprep/andeys/AndeysApplication.java
@@ -0,0 +1,13 @@
+package com.examprep.andeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndeysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndeysApplication.class, args);
+    }
+
+}
---
+++ SpringMVC/ExamPrep02/src/test/java/com/examprep/andeys/AndeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examprep.andeys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndeysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
